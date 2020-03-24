50eadc7909c47b26e6576300a01916d847a65d5c
pvelasco <pablo.velasco@nyu.edu>
2020-03-24 14:46:12 -0400

    Removes Python 3.5
    It doesn't pass the tests, so I just remove support for it.

--- bidsphysio/info.py
+++ bidsphysio/info.py
@@ -10,14 +10,13 @@ CLASSIFIERS = [
     'Environment :: Console',
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Topic :: Scientific/Engineering'
 ]
 
-PYTHON_REQUIRES = ">=3.5"
+PYTHON_REQUIRES = ">=3.6"
 
 REQUIRES = [
     'numpy >= 1.17.1',
