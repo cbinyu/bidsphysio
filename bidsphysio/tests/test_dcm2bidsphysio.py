1d329909d5a40fc28641f91846442ed9ce4cc929
pvelasco <pablo.velasco@nyu.edu>
2020-03-30 17:18:38 -0400

    Adds tests for "dcm2bids" method
    For now, it just checks the json files, not the data proper

--- bidsphysio/tests/test_dcm2bidsphysio.py
+++ bidsphysio/tests/test_dcm2bidsphysio.py
@@ -7,14 +7,16 @@ from .utils import TESTS_DATA_PATH
 
 import pytest
 import sys
-
+from pathlib import Path
 
 '''
 TO-DO:
 
-- Tests for "main":
-    * test arguments
-    * test mkdir
+- Set the expected signals (and maybe file contents) from the
+  tests data in a separate file , with the data, which the test
+  functions will read. That way, if we change the tests datasets,
+  we will change the expected values right there, rather than
+  changing the tests in this file
 
 - Tests "dcm2bids":
   Using the real file, check the output matches what we expect:
@@ -43,6 +45,7 @@ def mock_dcm2bidsphysio(monkeypatch):
        before the call to dcm2bids
     """
     def mock_dcm2bids(*args, **kwargs):
+        print('mock_dcm2bids called')
         return
 
     monkeypatch.setattr(d2bp, "dcm2bids", mock_dcm2bids)
@@ -54,7 +57,8 @@ def mock_dcm2bidsphysio(monkeypatch):
 def test_main_args(
         monkeypatch,
         tmpdir,
-        mock_dcm2bidsphysio
+        mock_dcm2bidsphysio,
+        capfd
 ):
     '''
     Tests for "main"
@@ -62,10 +66,10 @@ def test_main_args(
     separated function
     '''
     # 1) "infile" doesn't exist:
-    infile = tmpdir / 'boo.dcm'
+    infile = str(tmpdir / 'boo.dcm')
     args = (
         'dcm2bidsphysio -i {infile} -b {bp}'.format(
-            infile=str(infile),
+            infile=infile,
             bp=tmpdir / 'mydir' / 'foo'
         )
     ).split(' ')
@@ -73,14 +77,59 @@ def test_main_args(
     with pytest.raises(FileNotFoundError) as e_info:
         d2bp.main()
         assert str(e_info.value).endswith(' file not found')
-        assert str(e_info.value).split(' file not found')[0] == str(infile)
+        assert str(e_info.value).split(' file not found')[0] == infile
 
     # 2) "infile" does exist, but output directory doesn't exist:
     #    The output directory should be created and the "dcm2bids" function should be called
     args[ args.index('-i')+1 ] = str(TESTS_DATA_PATH / 'samplePhysio+02+physio_test+00001.dcm')
     monkeypatch.setattr(sys, 'argv',args)
     d2bp.main()
-
     assert (tmpdir / 'mydir').exists()
-    #assert False
-    
+    assert capfd.readouterr().out == 'mock_dcm2bids called\n'
+
+def test_dcm2bids(
+        monkeypatch,
+        tmpdir,
+        capfd
+):
+    '''
+    Tests for the call to "dcm2bids"
+    We will call it by calling "main" to make sure the output directory
+    is created, etc.
+    '''
+    import json
+
+    infile = str(TESTS_DATA_PATH / 'samplePhysio+02+physio_test+00001.dcm')
+    outbids = str(tmpdir / 'mydir' / 'bids')
+
+    args = (
+        'dcm2bidsphysio -i {infile} -b {bp}'.format(
+            infile=str(infile),
+            bp=outbids
+        )
+    ).split(' ')
+    monkeypatch.setattr(sys, 'argv',args)
+
+    # call "main" (which will create the output dir and call "dcm2bids"):
+    d2bp.main()
+
+    # make sure we are not calling the mock_dcm2bidds, but the real one:
+    assert capfd.readouterr().out != 'mock_dcm2bids called\n'
+
+    # Check that we have as many signals as expected
+    json_files = sorted(Path(tmpdir / 'mydir').glob('*.json'))
+    assert len(json_files)==2
+    for s in ['respiratory', 'cardiac']:
+        expectedFileName = Path(outbids).name + '_recording-' + s + '_physio.json'
+        jsonFile = tmpdir / 'mydir' / expectedFileName
+        assert jsonFile in json_files
+
+        # check content of the json file:
+        with open(jsonFile) as f:
+            d = json.load(f)
+            assert d['Columns'] == [ s, 'trigger']
+            assert d['StartTime'] == -1.632
+            if s == 'respiratory':
+                assert d['SamplingFrequency'] == 125
+            elif s == 'cardiac':
+                assert d['SamplingFrequency'] == 500
