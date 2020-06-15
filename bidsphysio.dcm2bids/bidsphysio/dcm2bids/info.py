cb2895439c5ba7d3d012135238a9beca809682ad
pvelasco <pablo.velasco@nyu.edu>
2020-06-15 16:50:33 -0400

    RF: Splitting dcm2bids as a subpackage
    RF: bidsphysio.dcm2bids subpackage

---
+++ bidsphysio.dcm2bids/bidsphysio/dcm2bids/info.py
@@ -0,0 +1,37 @@
+__version__ = "0.0.1"
+__author__ = "Pablo Velasco"
+__author_email__ = "pablo.velasco@nyu.edu"
+__url__ = "https://github.com/cbinyu/bidsphysio"
+__packagename__ = 'bidsphysio.dcm2bids'
+__description__ = "Physio-DICOM-to-BIDS Converter"
+__license__ = "MIT"
+__longdesc__ = """Converts physio data from a CMRR physiology DICOM file to BIDS physiological recording."""
+
+CLASSIFIERS = [
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Topic :: Scientific/Engineering'
+]
+
+PYTHON_REQUIRES = ">=3.6"
+
+REQUIRES = [
+    'bidsphysio.base',
+    'numpy >= 1.17.1',
+    'pydicom >= 1.4.1',
+]
+
+TESTS_REQUIRES = [
+    'pytest'
+]
+
+EXTRA_REQUIRES = {
+    'tests': TESTS_REQUIRES,
+}
+
+# Flatten the lists
+EXTRA_REQUIRES['all'] = sum(EXTRA_REQUIRES.values(), [])
