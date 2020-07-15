c0a20e6755192e0702bb555147617bb7ebf8d6eb
pvelasco <pablo.velasco@nyu.edu>
2020-07-15 14:08:33 -0400

    Minor changes to "test_session2bids"
     * Improves comments and docstrings
     * Defines a function with mocks that are used by other functions, so we don't repeat code

--- bidsphysio.session/tests/test_session2bids.py
+++ bidsphysio.session/tests/test_session2bids.py
@@ -1,4 +1,12 @@
-"""   Tests for the module "session2bids.py"   """
+"""   Tests for the module "session2bids.py"
+
+Because the functionality of extracting BIDS info from a session and
+getting physio data from a file, etc. is not the scope of the
+module, I'm mocking a lot of the functions and I just test the code
+specific to the module
+
+Also, this way allows us to run the tests without a dataset.
+"""
 
 from datetime import datetime, timedelta
 from os.path import join as pjoin
@@ -15,11 +23,11 @@ from bidsphysio.session import session2bids as s2b
 from .utils import TESTS_DATA_PATH, file_md5sum
 
 NOW = datetime.now()
-# physio clock is delay w.r.t. the scanner clock:
+# physio clock delay w.r.t. the scanner clock:
 DELAY = 10 * random.random()
 TIME_BETWEEN_RUNS = 100
 N_RUNS = 3
-# We'll add some jitter in the beginning of the physio recordings:
+# We'll add some jitter to the beginning of the physio recordings:
 JITTER_FACTOR = 0.1
 TR = 3 * random.random()
 N_VOLS = round(100 * random.random())
@@ -35,6 +43,8 @@ class MockBidsFile(object):
 
     @staticmethod
     def get_metadata():
+        """ Returns a dictionary with the needed keys
+        """
         return {
             'RepetitionTime': TR,
             'AcquisitionTime': str(timedelta(seconds=random.random() * SECS_IN_DAY))
@@ -45,19 +55,17 @@ def mock_layout_init(*args, **kwargs):
     """ Mocks the __init__ method of BIDSLayout
     This way we don't run the verifications normally run at init
     """
+    pass
 
 
 def mock_layout_get(*args, **kwargs):
     """ Mocks the BIDSLayout.get method to get the files
     It returns a list of MockBidsFiles
     """
-    myfiles = [
-        MockBidsFile(
-            path='scan_{}'.format(i)
-        )
-        for i in range(N_RUNS + 1)    # so that we have an extra file
-    ]
-    return myfiles
+    # We add one extra file ("N_RUNS + 1") so that we can have a BIDS
+    # file that doesn't exist in the "scan_df" dataframe and test a
+    # special case in "determine_scan_durations"
+    return [MockBidsFile(path='scan_{}'.format(i)) for i in range(N_RUNS + 1)]
 
 
 def mock_nibabel_load(*args, **kwargs):
@@ -81,6 +89,15 @@ class MockPhysioData(object):
     @staticmethod
     def save_to_bids_with_trigger(path):
         pass
+
+@pytest.fixture
+def my_mocks(monkeypatch):
+    """ Applies the monkeypatches for BIDSLayout.__init__ and .get,
+    as well as for nibabel.load
+    """
+    monkeypatch.setattr(BIDSLayout, "__init__", mock_layout_init)
+    monkeypatch.setattr(BIDSLayout, "get", mock_layout_get)
+    monkeypatch.setattr(nib, "load", mock_nibabel_load)
 
 
 @pytest.fixture
@@ -106,7 +123,8 @@ def myphysiodf(scope="module"):
 
 @pytest.fixture
 def myscandf(scope="module"):
-    """   Create a DataFrame with simulated scan content  """
+    """   Create a DataFrame with simulated scan content
+    """
     return pd.DataFrame(
         {
             'filename': ['scan_{}'.format(i) for i in range(N_RUNS)],
@@ -116,8 +134,7 @@ def myscandf(scope="module"):
 
 
 def test_compress_physio(tmpdir):
-    """
-    Tests for "compress_physio"
+    """ Tests for "compress_physio"
     We want to ensure reproducibility
     """
 
@@ -125,8 +142,8 @@ def test_compress_physio(tmpdir):
     out_prefix = str(tmpdir.join("precious"))
 
     def get_physio_acq_time(file):
-        """
-        Pretend we run get_physio_acq_time, but just return a fix date
+        """ Pretend we run get_physio_acq_time, but just return a
+        fix date.
         This allows us to run the test without the bioread dependency
         """
         return NOW
@@ -148,7 +165,8 @@ def test_compress_physio(tmpdir):
     # reset overwrite
     args[-1] = True
 
-    # make sure when run on the same file, we get identical results
+    # make sure when run on the same file at a later time, we get
+    # identical results
     time.sleep(1.1)  # need to guarantee change of time
     tarball_ = s2b.compress_physio(*args)
     md5_ = file_md5sum(tarball_)
@@ -164,7 +182,8 @@ def test_synchronize_onsets(
     """    Tests for the call to "synchronize_onsets"
     """
     out_df = s2b.synchronize_onsets(myphysiodf, myscandf)
-    # capture output and make sure estimated delay is close to "delay":
+    # capture the output and make sure estimated delay is close to
+    # "DELAY":
     for t in capfd.readouterr().out.split():
         try:
             estimated_delay = float(t)
@@ -177,7 +196,8 @@ def test_synchronize_onsets(
     for i in range(N_RUNS):
         physio_name = 'foo_{}'.format(i)
         scan_fname = out_df.scan_fname[out_df.filename == physio_name].to_string(index=False)
-        # for some reason, to_string adds a space before the string, so split()
+        # for some reason, ".to_string" adds a space before the
+        # string, so split() to remove it
         assert scan_fname.split()[0] == 'scan_{}'.format(i)
 
 
@@ -185,12 +205,15 @@ def test_plot_sync(
         myphysiodf,
         myscandf,
 ):
+    """   Tests for "plot_sync"
+    """
     # if physio data has not been synchronized yet, get an error:
     with pytest.raises(RuntimeError) as e_info:
         s2b.plot_sync(myscandf, myphysiodf)
     assert str(e_info.value) == 'The physio data has not been synchronized yet.'
 
-    # add a dummy 'phys_onset' to scan dataframe, and 'duration' to both
+    # add a dummy 'phys_onset' to scan dataframe, and 'duration' to
+    # both, so it looks like we did run "synchronize_onsets":
     myscandf['phys_onset'] = [i for i in range(N_RUNS)]
     myscandf['duration'] = myphysiodf['duration'] = [0.9*TIME_BETWEEN_RUNS] * N_RUNS
     myfig, myaxes = s2b.plot_sync(myscandf, myphysiodf)
@@ -200,16 +223,11 @@ def test_plot_sync(
 
 def test_determine_scan_durations(
         myscandf,
-        monkeypatch,
+        my_mocks,
         capfd
 ):
-    """ Tests for determine_scan_durations.
+    """   Tests for "determine_scan_durations"
     """
-
-    # apply the monkeypatches for BIDSLayout.__init__ and .get
-    monkeypatch.setattr(BIDSLayout, "__init__", mock_layout_init)
-    monkeypatch.setattr(BIDSLayout, "get", mock_layout_get)
-    monkeypatch.setattr(nib, "load", mock_nibabel_load)
 
     layout = BIDSLayout()
     subject = '01'
@@ -217,26 +235,22 @@ def test_determine_scan_durations(
 
     mynewscandf = s2b.determine_scan_durations(layout, myscandf, subject, session)
 
-    # make sure we skip files that are in the 'layout' but not in 'myscandf':
+    # make sure we skip files that are in the 'layout' but not in
+    # 'myscandf' (that will be the file 'scan_X', with X=N_RUNS):
     out = capfd.readouterr().out
     assert ('Skipping' in out and str(N_RUNS) in out)
 
-    # make sure mynewfscandf has a "duration" column with the name of
-    # scan filenames:
+    # make sure mynewfscandf has a "duration" column with the
+    # correct duration:
     for dur in mynewscandf['duration']:
         assert dur == pytest.approx(TR*N_VOLS)
 
 
 def test_load_scan_data(
-        monkeypatch,
+        my_mocks,
 ):
-    """ Tests for load_scan_data.
+    """ Tests for "load_scan_data"
     """
-
-    # apply the monkeypatches for BIDSLayout.__init__ and .get
-    monkeypatch.setattr(BIDSLayout, "__init__", mock_layout_init)
-    monkeypatch.setattr(BIDSLayout, "get", mock_layout_get)
-    monkeypatch.setattr(nib, "load", mock_nibabel_load)
 
     layout = BIDSLayout()
     subject = '01'
@@ -244,28 +258,28 @@ def test_load_scan_data(
 
     scandf = s2b.load_scan_data(layout, subject, session)
 
-    # make sure scandf has the columns: "acq_time", "duration" and "onset"
+    # make sure scandf has the columns: "acq_time", "duration" and
+    # "onset":
     for col_name in ['acq_time', 'duration', 'onset']:
         assert col_name in scandf.columns
 
 
 def test_convert_session(
         monkeypatch,
+        my_mocks,
 ):
-    """ Tests for load_scan_data.
+    """ Tests for "load_scan_data"
     """
 
     def mock_compress_physio(*args, **kwargs):
         pass
 
-    # apply the monkeypatches for BIDSLayout.__init__ and .get
-    monkeypatch.setattr(BIDSLayout, "__init__", mock_layout_init)
-    monkeypatch.setattr(BIDSLayout, "get", mock_layout_get)
-    monkeypatch.setattr(nib, "load", mock_nibabel_load)
+    # apply the monkeypatch for "compress_physio":
     monkeypatch.setattr(s2b, "compress_physio", mock_compress_physio)
 
     def _get_physio_data(fname):
-        """ Function to return a MockPhysioData object """
+        """ Function to return a MockPhysioData object
+        """
         return MockPhysioData(path=fname)
 
     def _get_physio_acq_time(fname):
@@ -278,7 +292,6 @@ def test_convert_session(
             raise FileNotFoundError('This is not a valid filename')
         return timedelta(seconds=run_no * TIME_BETWEEN_RUNS)
 
-
     # run convert_session:
     phys_files = ['phys_{}'.format(i) for i in range(N_RUNS)]
     bids_dir = 'foo'
@@ -287,4 +300,3 @@ def test_convert_session(
                         get_physio_data=_get_physio_data,
                         get_physio_acq_time=_get_physio_acq_time,
                         outdir='bar', overwrite=True)
-    
