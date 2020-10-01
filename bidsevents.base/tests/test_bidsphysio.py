dcbe46eac71d2d684fe07f710a227221be2920a3
Chrysa Papadaniil <chrysa@b-10-27-80-105.dynapool.vpn.nyu.edu>
2020-10-01 12:40:31 -0400

    changed name of events subpackage

--- bidsevents.base/tests/test_bidsphysio.py
+++
@@ -1,9 +0,0 @@
-"""   Tests for the module "bidsevents.py"   """
-
-import numpy as np
-import pytest
-
-from bidsphysio.base.bidsevents import (EventSignal,
-                                        EventData)
-
-#In progress...
