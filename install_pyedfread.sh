e7e193e18e3e9d470334eee7444aac9fcb80f538
Chrysa Papadaniil <chrysa@CBI-MacBook-Pro.lan1>
2020-10-06 14:58:49 -0400

    updated install_pyedfread.sh

--- install_pyedfread.sh
+++ install_pyedfread.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -ex
-wget https://github.com/nwilming/pyedfread/archive/master.tar.gz
-tar -xzvf pyedfread-master.tar.gz
-cd pyedfread-master && sed -i -e "s/cython: profile=True/cython: profile=True, language_level=2/" pyedfread/edfread.pyx && python setup.py install
+set -e
+git clone https://github.com/nwilming/pyedfread.git
+cd pyedfread && sed -i -e "s/cython: profile=True/cython: profile=True, language_level=2/" pyedfread/edfread.pyx && python setup.py install
