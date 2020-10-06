4a44c688b8ef48b5943353b1fb8b99a7ee233cf6
Chrysa Papadaniil <chrysa@CBI-MacBook-Pro.lan1>
2020-10-06 15:20:22 -0400

    Make install_pyedfread.sh executable

--- install_pyedfread.sh
+++ install_pyedfread.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 git clone https://github.com/nwilming/pyedfread.git
-cd pyedfread && sed -i -e "s/cython: profile=True/cython: profile=True, language_level=2/" pyedfread/edfread.pyx && python setup.py install
+cd pyedfread && python setup.py install
